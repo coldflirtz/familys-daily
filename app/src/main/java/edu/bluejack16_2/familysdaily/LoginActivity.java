0446661ca927264148fcd21eb2ead3d53879a70f
coldflirtz <fidelisandryanushm@live.com>
2017-07-10 15:16:41 +0700

    Add Google Login + Floating Action Menu + Fixed Register Activity background bug.

--- app/src/main/java/edu/bluejack16_2/familysdaily/LoginActivity.java
+++ app/src/main/java/edu/bluejack16_2/familysdaily/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,9 +13,18 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -23,13 +33,21 @@ import com.google.firebase.auth.FacebookAuthCredential;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserInfo;
 
-public class LoginActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
     private LoginButton fbLoginBtn;
+    private SignInButton googleLoginBtn;
     private CallbackManager callbackManager;
     private TextView tvTest, tvRegister;
     private FirebaseAuth firebaseAuth;
+    private GoogleApiClient googleApiClient;
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +67,11 @@ public class LoginActivity extends AppCompatActivity {
         tvTest = (TextView) findViewById(R.id.tvTest);
         tvRegister = (TextView)findViewById(R.id.tvRegister);
         fbLoginBtn.setReadPermissions("email", "public_profile");
+        googleLoginBtn = (SignInButton) findViewById(R.id.googleLoginBtn);
+        googleLoginBtn.setOnClickListener(this);
         firebaseAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.google_web_client_id)).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
         Toast.makeText(this, "init success", Toast.LENGTH_SHORT).show();
     }
 
@@ -73,6 +95,11 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void googleLogin(){
+        Intent loginIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(loginIntent, RC_SIGN_IN);
+    }
+
     private void goToRegister(){
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,19 +120,36 @@ public class LoginActivity extends AppCompatActivity {
     private void updateUI(FirebaseUser user){
         if(user != null){
             tvTest.setText(user.getDisplayName()+"\n"+user.getUid());
+            /*String userProvider = user.getProviderId();
+            if(userProvider == "facebook.com") {
+                for(UserInfo userInfo : user.getProviderData()){
+                    GraphRequest request = GraphRequest.newMeRequest();
+                }
+
+            }else if(userProvider == "google.com"){
+
+            }*/
+
         }
         else{
             tvTest.setText(null);
         }
     }
 
-    private void handleFacebookAccessToken(AccessToken token){
+    private void handleFacebookAccessToken(final AccessToken token){
         AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
         firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     FirebaseUser user = firebaseAuth.getCurrentUser();
+                    GraphRequest request = GraphRequest.newMeRequest(token, new GraphRequest.GraphJSONObjectCallback(){
+
+                        @Override
+                        public void onCompleted(JSONObject object, GraphResponse response) {
+
+                        }
+                    });
                     updateUI(user);
                 }
                 else{
@@ -120,5 +164,52 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
+            else {
+                updateUI(null);
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        Log.d(TAG, "firebaseAuthWithGoogle:"+account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    updateUI(user);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if(i==R.id.googleLoginBtn){
+            googleLogin();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Google Play Service error.", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/edu/bluejack16_2/familysdaily/RegisterActivity.java
+++ app/src/main/java/edu/bluejack16_2/familysdaily/RegisterActivity.java
@@ -1,15 +1,17 @@
 package edu.bluejack16_2.familysdaily;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -23,7 +25,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         init();
-
     }
 
     private void init(){
@@ -46,9 +47,23 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+
     private void setGenderSpinner(){
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, new String[]{"Gender", "Male","Female"});
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, new String[]{"Gender", "Male","Female"});
         spGender.setAdapter(spinnerAdapter);
+        /*View v = spGender.getSelectedView();
+        ((TextView) v).setTextColor(Color.WHITE);
+        spGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) view).setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });*/
     }
 
     private void onRegisterClicked(){
